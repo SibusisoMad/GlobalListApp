744e1d3fb1beeabe1512c7ad932ec36d2fccd762
Sibusiso Madloba <smadloba@gmail.com>
2023-08-04 21:14:07 +0200

    Adding Serializer class

---
+++ GlobalListUtility/Serialization/Serializer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace GlobalListUtility.Serialization
+{
+    public static class Serializer
+    {
+        public static void SerializeToBinary(List<int> list, string fileName)
+        {
+            using (FileStream fileStream = new FileStream(fileName, FileMode.Create))
+            {
+                using (BinaryWriter writer = new BinaryWriter(fileStream))
+                {
+                    foreach (int number in list)
+                    {
+                        writer.Write(number);
+                    }
+                }
+            }
+        }
+
+        public static void SerializeToXml(List<int> list, string fileName)
+        {
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(List<int>));
+
+                serializer.Serialize(writer, list);
+            }
+        }
+    }
+}
