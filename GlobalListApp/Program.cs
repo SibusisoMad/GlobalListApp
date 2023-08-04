4a4755ffcb5ebe033228c7f9dcba79e1166d2653
Sibusiso Madloba <smadloba@gmail.com>
2023-08-04 23:18:06 +0200

    ErrorHandling(added serilog for error loggin)

--- GlobalListApp/Program.cs
+++ GlobalListApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Serilog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -12,46 +13,63 @@ namespace GlobalListApp.Threads
         public static object lockObject = new object();
         public static bool stopThreads = false;
 
+        private static readonly ILogger Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .WriteTo.File("GlobalListLog.txt", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+
+
         public static void Main(string[] args)
         {
-            Thread oddNumberThread = new Thread(GenerateOddNumbers.Run);
-            Thread primeNumberThread = new Thread(GeneratePrimeNumbers.Run);
-            Thread evenNumberThread = new Thread(GenerateEvenNumbers.Run);
-
-            oddNumberThread.Start();
-            primeNumberThread.Start();
-
-            while (globalList.Count < 250000)
+            try
             {
-                Thread.Sleep(100);
+
+                Thread oddNumberThread = new Thread(GenerateOddNumbers.Run);
+                Thread primeNumberThread = new Thread(GeneratePrimeNumbers.Run);
+                Thread evenNumberThread = new Thread(GenerateEvenNumbers.Run);
+
+                oddNumberThread.Start();
+                primeNumberThread.Start();
+
+                while (globalList.Count < 250000)
+                {
+                    Thread.Sleep(100);
+                }
+
+                evenNumberThread.Start();
+
+                while (totalCount < 1000000)
+                {
+                    Thread.Sleep(100);
+                }
+
+                stopThreads = true;
+
+                oddNumberThread.Join();
+                primeNumberThread.Join();
+                evenNumberThread.Join();
+
+                List<int> sortedList = globalList.OrderBy(num => num).ToList();
+
+                int oddCount = sortedList.Count(num => num % 2 != 0);
+                int evenCount = sortedList.Count(num => num % 2 == 0);
+
+                Console.WriteLine("Odd Count: " + oddCount);
+                Console.WriteLine("Even Count: " + evenCount);
+
+                GlobalListUtility.Serialization.Serializer.SerializeToBinary(sortedList, "global_list.bin");
+                GlobalListUtility.Serialization.Serializer.SerializeToXml(sortedList, "global_list.xml");
+
+                Logger.Information("Serialization completed. Sorted List Count: {SortedListCount}, Odd Count: {OddCount}, Even Count: {EvenCount}", sortedList.Count, oddCount, evenCount);
+
+                Console.ReadLine();
+
             }
-
-            evenNumberThread.Start();
-
-            while (totalCount < 1000000)
+            catch (Exception ex)
             {
-                Thread.Sleep(100);
+                Logger.Error(ex, "An unhandled exception occurred: {ErrorMessage}", ex.Message);
             }
-
-            stopThreads = true;
-
-            oddNumberThread.Join();
-            primeNumberThread.Join();
-            evenNumberThread.Join();
-
-            List<int> sortedList = globalList.OrderBy(num => num).ToList();
-            int oddCount = sortedList.Count(num => num % 2 != 0);
-            int evenCount = sortedList.Count(num => num % 2 == 0);
-
-            Console.WriteLine("Odd Count: " + oddCount);
-            Console.WriteLine("Even Count: " + evenCount);
-
-            GlobalListUtility.Serialization.Serializer.SerializeToBinary(sortedList, "global_list.bin");
-            GlobalListUtility.Serialization.Serializer.SerializeToXml(sortedList, "global_list.xml");
-
-            Console.WriteLine("Serialization completed.");
-
-            Console.ReadLine();
+            
         }
 
     }
--- GlobalListUtility/Serialization/Serializer.cs
+++ GlobalListUtility/Serialization/Serializer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using Serilog;
+using System.Collections.Generic;
 using System.IO;
 using System.Xml.Serialization;
 
@@ -6,26 +7,54 @@ namespace GlobalListUtility.Serialization
 {
     public class Serializer
     {
+        private static readonly ILogger Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .WriteTo.File("GlobalListLog.txt", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
         public static void SerializeToBinary(List<int> list, string filePath)
         {
-            using (FileStream fileStream = new FileStream(filePath, FileMode.Create))
+
+            try
             {
-                using (BinaryWriter writer = new BinaryWriter(fileStream))
+                using (FileStream fileStream = new FileStream(filePath, FileMode.Create))
                 {
-                    foreach (int number in list)
+                    using (BinaryWriter writer = new BinaryWriter(fileStream))
                     {
-                        writer.Write(number);
+                        foreach (int number in list)
+                        {
+                            writer.Write(number);
+                        }
                     }
                 }
+
+                Logger.Information("Binary serialization completed successfully. File path: {FilePath}", filePath);
+
             }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, "Error occured during binary serialization");
+            }
+            
         }
 
         public static void SerializeToXml<T>(T data, string filePath)
         {
-            XmlSerializer serializer = new XmlSerializer(typeof(T));
-            using (TextWriter writer = new StreamWriter(filePath))
+
+            try
             {
-                serializer.Serialize(writer, data);
+
+                XmlSerializer serializer = new XmlSerializer(typeof(T));
+                using (TextWriter writer = new StreamWriter(filePath))
+                {
+                    serializer.Serialize(writer, data);
+                }
+
+                Logger.Information("Serialized object of type {Type} to XML: {FilePath}", typeof(T).Name, filePath);
+
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, "Error occured during XML serialization");
             }
         }
     }
