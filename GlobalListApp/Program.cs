14f6ec3f1f7d1ee4ecc2ac6ddb6c18e23e3a6e94
Sibusiso Madloba <smadloba@gmail.com>
2023-08-04 21:44:33 +0200

    Adding Thread classe GeneratePrimeNumbers

--- GlobalListApp/Program.cs
+++ GlobalListApp/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace GlobalListApp.Threads
+{
+   public class Program
+    {
+        public static List<int> globalList = new List<int>();
+        public static int totalCount = 0;
+        public static object lockObject = new object();
+        public static bool stopThreads = false;
+
+        static void Main(string[] args)
+        {
+            
+            Thread primeThread = new Thread(GeneratePrimeNumbers.Run);
+            primeThread.Start();
+
+            while (globalList.Count < 250000)
+            {
+                Thread.Sleep(100);
+            }
+
+            while (totalCount < 1000000)
+            {
+                Thread.Sleep(100);
+            }
+
+            stopThreads = true;
+
+            primeThread.Join();
+
+            List<int> sortedList = globalList.OrderBy(num => num).ToList();
+            int oddCount = sortedList.Count(num => num % 2 != 0);
+            int evenCount = sortedList.Count(num => num % 2 == 0);
+
+            Console.WriteLine("Odd Count: " + oddCount);
+            Console.WriteLine("Even Count: " + evenCount);
+
+            GlobalListUtility.Serialization.Serializer.SerializeToBinary(sortedList, "global_list.bin");
+            GlobalListUtility.Serialization.Serializer.SerializeToXml(sortedList, "global_list.xml");
+
+            Console.WriteLine("Serialization completed.");
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ GlobalListApp/Threads/GeneratePrimeNumbers.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalListApp.Threads
+{
+    public static class GeneratePrimeNumbers
+    {
+        public static void Run()
+        {
+            int number = 2;
+
+            while(!Program.stopThreads)
+            {
+                lock (Program.lockObject)
+                {
+                    if(IsPrime(number))
+                    {
+                        Program.globalList.Add(number);
+                        Program.totalCount++;
+                    }
+
+                    number++;
+
+                }
+
+            }
+        }
+
+        private static bool IsPrime(int number)
+        {
+            if(number < 2)
+                return false;
+
+            for(int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if(number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
